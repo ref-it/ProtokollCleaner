74192d60fd5740475d9e3c686f533376c10a1665
martin <martin@localhost.localdomain>
2017-10-24 01:24:57 +0200

    current State

---
+++ Main.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: martin
+ * Date: 18.10.17
+ * Time: 09:54
+ */
+
+class Main
+{
+    public static $inputpath = "/home/martin/test/intern/";
+    public static $outputpath = "/home/martin/test/public/";
+    private $startMonth = 01;
+    private $startYear  = 2016;
+    private $startday   = 01;
+    private $files;
+
+    function getAllFiles()
+    {
+        $this->files = array();
+        $alledateien = scandir(Main::$inputpath); //Ordner "files" auslesen
+        foreach ($alledateien as $datei) { // Ausgabeschleife
+            $length = strlen($datei);
+            if((substr($datei, 0,1) == ".") and (substr($datei, $length - 4, 4) != ".txt") and ($length < 5))
+            {
+                continue;
+            }
+            $Datum = $this->getDateFromFileName($datei);
+            if(intval($Datum->Year()) < $this->startYear)
+            {
+                continue;
+            }
+            if((intval($Datum->Year()) === $this->startYear) and (intval($Datum->Month()) < $this->startMonth))
+            {
+                continue;
+            }
+            if((intval($Datum->Year()) === $this->startYear) and (intval($Datum->Month()) === $this->startMonth) and (intval($Datum->Day()) < $this->startday))
+            {
+                continue;
+            }
+            $file = new File($Datum, $datei);
+            $fn = Main::$outputpath . "/" . $file->getOutputFilename();
+            $this->copy($file->getFilename(), $fn);
+
+            $this->files[] = $file;
+
+
+            echo $Datum->GermanDate() . "<br />";
+        }
+
+    }
+    function copy($fileName, $fn)
+    {
+        $OffRec=false;
+        $lines = array();
+        if ($fl = fopen($fileName, "r")) {
+            while(!feof($fl)) {
+                $line = fgets($fl);
+                # do same stuff with the $line
+                if(strpos($line, 'tag>intern') !== false) {
+                    $OffRec=true;
+                    continue;
+                }
+                if(!$OffRec)
+                {
+                    $lines[] = $line;
+                    continue;
+                }
+                if($OffRec and strpos($line, 'tag>nointern') !== false) {
+                    $OffRec=false;
+                }
+
+            }
+            fclose($fl);
+        }
+        if($fl = fopen($fn, "w+")) {
+            foreach ($lines as $line) {
+                fwrite($fl, $line);
+                echo $line . "<br />";
+            }
+        }
+        fclose($fl);
+    }
+
+    function getDateFromFileName($Filename)
+    {
+        $length = strlen($Filename);
+        $Name = substr($Filename, 0, $length - 4);
+        $d = substr($Name, 8, 2);
+        $m = substr($Name, 5, 2);
+        $y = substr($Name, 0, 4);
+        $Date = new Date($y,$m,$d);
+        return $Date;
+    }
+}
+
+class Date
+{
+    function __construct($Year, $Month, $Day)
+    {
+        $this->y = $Year;
+        $this->m = $Month;
+        $this->d = $Day;
+    }
+
+    private $y;
+    private $m;
+    private $d;
+
+    function Year()
+    {
+        return $this->y;
+    }
+    function Month()
+    {
+        return $this->m;
+    }
+    function Day()
+    {
+        return $this->d;
+    }
+    function GermanDate()
+    {
+        return $this->d . "." . $this->m . "." . $this->y;
+    }
+    function Filname()
+    {
+        return $this->y . "-" . $this->m . "-" . $this->d . ".txt";
+    }
+
+}
+class File
+{
+    private $datum;
+    private $Filename;
+
+    function __construct($date, $file)
+    {
+        $this->datum = $date;
+        $this->Filename = Main::$inputpath . "/" . $file;
+    }
+
+    function getDate()
+    {
+        return $this->datum;
+    }
+    function getFilename()
+    {
+        return $this->Filename;
+    }
+    function getOutputFilename()
+    {
+        return $this->datum->Filname();
+    }
+}
+$haupt = new Main();
+
+$haupt->getAllFiles();
+
+
+?>
+</body>
+</html>
