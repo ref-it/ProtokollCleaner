7f002501e4852f90911ccae9d42f71a4e666e350
martin <martin.sebastian@live.de>
2017-11-12 19:30:23 +0100

    Example for Testing

--- Main.php
+++ Main.php
@@ -30,7 +30,42 @@ class Main
 
     public function __construct() // or any other method
     {
-        include 'config.php';
+        $switch = false;
+        $listFilesCurrentDir = scandir(getcwd());
+        foreach ($listFilesCurrentDir as $file)
+        {
+            if ($file === "config.php")
+            {
+                include 'config.php';
+                $switch = true;
+                break;
+            }
+        }
+        if ($switch === false) { //just for example parsing in files
+            Main::$inputpath  = getcwd() . "/examples/intern/"; //intern part of the Wiki, where the files which will be cleaned are
+            Main::$outputpath = getcwd() . "/examples/extern/"; //public part of the Wiki, where the cleaned files will be saved
+            Main::$decissionList = getcwd() . "/examples/beschluesse.txt"; //List off StuRa Decissions
+            Main::$helperFilePath = getcwd() . "/help.txt"; //List off StuRa Decissions
+            Main::$starttag   = "intern"; //start tag of cleaning area
+            Main::$endtag     = "nointern"; //end tag of cleaning area
+            Main::$debug = true ; //all as Text on Browser
+            Main::$onlyNew = false; //only new financial decissions
+            Main::$postData = false; //set to true if you want to post data to another website
+            Main::$PostUrl = "http://localhost"; //destination for Posting of financial decission list
+            Main::$startMonth = 01;    //Day,
+            Main::$startYear  = 2016;  //Month and
+            Main::$startday   = 01;    //Y
+        }
+        if (Main::$debug)
+        {
+            if ($switch == false) {
+                echo "Die Reserve-Config wurde genutzt. <br />" . PHP_EOL;
+            }
+            else
+            {
+                echo "Die Config wurde genutzt. <br />" . PHP_EOL;
+            }
+        }
     }
 
     public function getAllFiles()
