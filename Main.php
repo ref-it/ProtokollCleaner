60afa503b89104bb424eb807c73ff4bd4e2012f4
martin <martin.sebastian@live.de>
2017-11-18 17:32:14 +0100

    fix #14

--- Main.php
+++ Main.php
@@ -42,19 +42,7 @@ class Main
             }
         }
         if ($switch === false) { //just for example parsing in files
-            Main::$inputpath  = getcwd() . "/examples/intern/"; //intern part of the Wiki, where the files which will be cleaned are
-            Main::$outputpath = getcwd() . "/examples/extern/"; //public part of the Wiki, where the cleaned files will be saved
-            Main::$decissionList = getcwd() . "/examples/beschluesse.txt"; //List off StuRa Decissions
-            Main::$helperFilePath = getcwd() . "/examples/help.txt"; //List off StuRa Decissions
-            Main::$starttag   = "intern"; //start tag of cleaning area
-            Main::$endtag     = "nointern"; //end tag of cleaning area
-            Main::$debug = true ; //all as Text on Browser
-            Main::$onlyNew = false; //only new financial decissions
-            Main::$postData = false; //set to true if you want to post data to another website
-            Main::$PostUrl = "http://localhost"; //destination for Posting of financial decission list
-            Main::$startMonth = 01;    //Day,
-            Main::$startYear  = 2016;  //Month and
-            Main::$startday   = 01;    //Y
+            include 'config.default.php';
         }
         if (Main::$debug)
         {
