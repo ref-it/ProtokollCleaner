ae068f476d29e4ae0b6f6c11bb7ca4401930d084
martin <martin.sebastian@live.de>
2018-02-19 17:23:59 +0100

    #19 Klasse angelegt

---
+++ src/framework/lib/LegislaturCrawler.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * LegislaturCrawler.php
+ * @author Martin S.
+ * @author Stura - Referat IT <ref-it@tu-ilmenau.de>
+ * @since 19.02.18 16:23
+ */
+
+class LegislaturCrawler
+{
+
+    public function __construct()
+    {
+
+    }
+
+}
