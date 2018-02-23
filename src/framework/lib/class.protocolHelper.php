1b4e6630435de9dd2e83cd784a29e27194c046b6
Michael Gnehr <michael@gnehr.de>
2018-02-23 08:01:32 +0100

    update protocol diff

--- src/framework/lib/class.protocolHelper.php
+++ src/framework/lib/class.protocolHelper.php
@@ -33,12 +33,19 @@ class protocolHelper
 	{
 	}
 	
+	private $isLineError = false;
+	private $lineError = '';
+	
 	private static $tagRegex = '/(({{tag>[a-zA-Z0-9]+(_[a-zA-Z0-9]+)*([ ]*[a-zA-Z0-9]+(_[a-zA-Z0-9]+)*)*( )*}}|===== geschlossener Teil =====|===== öffentlicher Teil =====)+)/';
 	private static $oldTags = ['===== geschlossener Teil =====', '===== öffentlicher Teil ====='];
 	
-	public function parseProto($gremium, $protokoll){
+	
+	public function parseProto($gremium, $protokoll, $isdraft = false, $nopreview = false){
+		prof_flag('parseProto_start');
 		$x = new wikiClient(WIKI_URL, WIKI_USER, WIKI_PASSWORD, WIKI_XMLRPX_PATH);
+		prof_flag('get wikipage');
 		$a = $x->getPage(PROTOMAP[$gremium][0].':'.$protokoll);
+		prof_flag('got single page');
 		$p = NULL;
 		if ($a) {
 			$p = new Protocol($a);
@@ -47,20 +54,99 @@ class protocolHelper
 			return;
 		}
 		
+		$isInternal = false;	// dont copy internal parts to public part
+		$isLineError = false;	// found parsing error
+		$lastTagClosed = true; 	// prevent duplicate closing tags and closing internal part before opening
+		$writeDraftText = 1;
 		
-		$isInternal = false;
+		//only fill preview or output
+		if (!$nopreview) $p->preview = protocolDiff::generateHeader();
+		
 		
 		// parser main loop - loop throught $protocol lines
+		// create internal <==> external diff
 		foreach($p->text_a as $linenumber => $line){
+			//detect protocol head to insert draft state
+			if ($isdraft) {
+				if ($writeDraftText >= 3) {
+					if (!$nopreview) $p->preview .= protocolDiff::generateCopiedChangedLine('====== ENTWURF - PROTOKOLL ======');
+					else $p->external .= '====== ENTWURF - PROTOKOLL ======'."\n";
+					$isdraft = false;
+				}
+				if ($writeDraftText >= 2) {
+					if (strpos($line, "}}") !== false){
+						$writeDraftText = 3;
+					}
+				} else if ($writeDraftText >= 1){
+					if (strpos($line, "{{template>:vorlagen:Protokoll") !== false){
+						$writeDraftText = 2;
+					}
+				}
+			}
+			// detect nonpublic parts
 			$matches = [];
 			$match = preg_match(self::$tagRegex, $line, $matches, PREG_OFFSET_CAPTURE, 0);
-			while (preg_match === 1 && $matches[0][1]>=0){
-				
+			$matchcount = 0;
+			$changed = false; // don't allow other text on state changing lines + don' place internal external changing tags to external script
+			if ($match === 1 && $matches[0][1]>=0){
+				if ($matches[0][0] == self::$oldTags[0]){ // count opening and closing tags
+					$p->tags['old'] = isset($p->tags['old'])? $p->tags['old'] + 1 : 1;
+					$isInternal = true;
+					$changed = true;
+				} else if ($matches[0][0] == self::$oldTags[1]) { // count opening and closing tags
+					$p->tags['old'] = (isset($p->tags['old']) && $p->tags['old'] > 0)? $p->tags['old'] - 1 : 0;
+					$isInternal = false;
+					$changed = true;
+				} else {  // count opening and closing tags //split found tags
+					$m = $matches[0][0]; // clean matches ...
+					$m = str_replace(['{{tag>', '  ', '}}'], ['',' ',''], $m);
+					$m = trim(str_replace(['{{tag>', '  ', '}}'], ['',' ',''], $m));
+					$m = explode(' ', $m);
+					foreach ($m as $single_tag) { //handle multiple tags inside tag area
+						if (strlen($single_tag)>2 && substr($single_tag,0, 2) != 'no') {
+							$p->tags[$single_tag] = isset($p->tags[$single_tag])? $p->tags[$single_tag] + 1 : 1;
+							if ($single_tag.PROTO_INTERNAL_TAG) $isInternal = true;
+							$changed = true;
+						} else {
+							$p->tags[$single_tag] = (isset($p->tags[$single_tag]) && $p->tags[$single_tag] > 0)? $p->tags[$single_tag] - 1 : 0;
+							if($single_tag == 'no'.PROTO_INTERNAL_TAG) $isInternal = false;
+							$changed = true;
+						}
+					}
+				}
+				if ($changed) { // internal/external state changed - test for errors
+					$tmp_line = trim(str_replace($matches[0][0], '', $line));
+					if ($tmp_line != ''){
+						$this->isLineError = true;
+						$this->lineError = "Please use a new line to seperate internal and external parts.";
+						if (!$nopreview) $p->preview .= protocolDiff::generateErrorChangedLine($line);
+						break;
+					}
+					if ($lastTagClosed == !$isInternal){
+						$this->isLineError = true;
+						$this->lineError = "Duplicate closing tag or closing before opening found.";
+						if (!$nopreview) $p->preview .= protocolDiff::generateErrorChangedLine($line);
+						break;
+					} else {
+						$lastTagClosed = !$isInternal;
+					}
+				}
 			}
-			
+			if ($isInternal || $changed){ // mark changes on preview
+				if (!$nopreview) $p->preview .= protocolDiff::generateRemovedLine($line);
+			} else { // only copy public lines
+				if (!$nopreview) $p->preview .= protocolDiff::generateCopiedLine($line);
+				else $p->external .= "$line\n";
+			}
+		}
+		if ($this->isLineError == true){ // error handling: show error to user
+			if (!$nopreview) $p->preview .= protocolDiff::generateErrorLine($this->lineError);
+			else $p->external .= protocolDiff::generateErrorChangedLine($this->lineError);
+		}
 		
 		
 		
+		/*
 		$protodiff = protocolDiff::generateHeader();
 		$OffRec = false;
 		$countInTag = 0;
@@ -111,10 +197,14 @@ class protocolHelper
 				$protodiff .= protocolDiff::generateRemovedLine($line);
 		
 		}
+		*/
+		$p->preview .= protocolDiff::generateFooter();
 		
-		$protodiff .= protocolDiff::generateFooter();
 		
-		echo $protodiff;
+
+		prof_flag('parseProto_end');
+		// return pointer to protocol object
+		return $p;
 		
 		//echo '<pre>'; var_dump($p); echo '</pre>';
 		//TODO open and close tags
@@ -122,6 +212,7 @@ class protocolHelper
 		//TODO detect todos
 		//TODO detect resolutions
 		//TODO cleanup array
+		//TODO check attachements
 		echo 'run parser';
 	}
 }
