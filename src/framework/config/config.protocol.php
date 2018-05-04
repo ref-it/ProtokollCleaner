5eeda82e864b0f496c185a031620d36f1511f6b8
Michael Gnehr <michael@gnehr.de>
2018-05-04 18:38:36 +0200

    update protocol configuration

--- src/framework/config/config.protocol.php
+++ src/framework/config/config.protocol.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * protokollmap 
- * 	permission: in array ;; out array ;; beschlussliste
+ * 	permission: in array ;; out array ;; beschlussliste ;; invitation mail target
  * @var array
  */
 define('PROTOMAP', [
-	'stura' => ['protokoll:stura:intern', 'spielwiese:test:public2', 'spielwiese:test:beschlussliste']
+	'stura' => ['protokoll:stura:intern', 'protokoll:stura', 'stura:intern:beschluesse', 'stura@tu-ilmenau.de']
 ]);
 
 define('PROTO_INTERNAL_TAG', 'intern'); //negative tag is always no[tag]; here: nointern
