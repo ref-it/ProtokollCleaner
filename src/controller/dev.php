af73e870c9158795b289d6bdcda27ffaedb1e8b1
Michael Gnehr <michael@gnehr.de>
2023-08-27 23:11:42 +0200

    add readonly wiki test content to Dev->WikiTest
    Signed-off-by: Michael Gnehr <michael@gnehr.de>

--- src/controller/dev.php
+++ src/controller/dev.php
@@ -30,6 +30,28 @@ class DevController extends MotherController
 	 * ACTION wiki
 	 */
 	public function wiki(){
+		$this->t->printPageHeader();
+		require_once (SYSBASE.'/framework/class.wikiClient.php');
+
+		$x = new wikiClient(WIKI_URL, WIKI_USER, WIKI_PASSWORD, WIKI_XMLRPX_PATH);
+
+		echo '<pre>'; var_dump('wiki time', $x->getTime()); echo '</pre><br><hr>';
+		echo '<pre>'; var_dump('wiki title', $x->getTitle()); echo '</pre><br><hr>';
+		echo '<pre>'; var_dump('wiki rpc', $x->getXMLRPCAPIVersion()); echo '</pre><br><hr>';
+		echo '<pre>'; var_dump('wiki version', $x->getVersion()); echo '</pre><br><hr>';
+		echo '<pre>'; var_dump('wiki page List', $x->getPagelist('protokoll')); echo '</pre><br><hr>';
+		echo '<pre>'; var_dump('wiki stura page html',
+			$x->getPageHTML('protokoll:stura')
+		); echo '</pre><br><hr>';
+		echo '<pre>'; var_dump('wiki, list stura attachement',
+			$x->listAttachements('protokoll:stura')
+		); echo '</pre><br><hr>';
+		echo '<pre>'; var_dump('wiki, list spielwiese attachement',
+			$x->listAttachements('spielwiese:test:utf8test')
+		); echo '</pre><br><hr>';
+
+		$this->t->printPageFooter();
+
 		/*
 		$this->t->printPageHeader();
 		require_once (SYSBASE.'/framework/class.wikiClient.php');
